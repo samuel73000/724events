2361719728f318a52aaf8e1bd60bb8ac99612de7
samuel73000 <50187969+samuel73000@users.noreply.github.com>
2023-07-18 11:56:39 +0200

    724events

--- src/containers/Slider/index.js
+++ src/containers/Slider/index.js
@@ -9,18 +9,26 @@ const Slider = () => {
   const { data } = useData();
   const [index, setIndex] = useState(0);
   const byDateDesc = data?.focus.sort((evtA, evtB) =>
-    new Date(evtA.date) < new Date(evtB.date) ? -1 : 1
+    new Date(evtA.date) > new Date(evtB.date) ? -1 : 1
   );
   const nextCard = () => {
     setTimeout(
-      () => setIndex(index < byDateDesc.length ? index + 1 : 0),
+      () => setIndex(index < byDateDesc.length-1 ? index + 1 : 0),
       5000
     );
   };
   useEffect(() => {
     nextCard();
   });
+
+
+
+
+
+
+
   return (
+    
     <div className="SlideCardList">
       {byDateDesc?.map((event , idx) => (
         <>
@@ -41,15 +49,18 @@ const Slider = () => {
           </div>
           <div className="SlideCard__paginationContainer">
             <div className="SlideCard__pagination">
-              {byDateDesc.map((_, radioIdx )  => (
+            {byDateDesc?.map((_, radioIdx )  => (
                  
-                <input
-                  key={`${event.id}`}
-                  type="radio"
-                  name="radio-button"
-                  checked= {idx === radioIdx}
-                />
-              ))}
+                 <input
+                 key={`${event.id}`}
+                   type="radio"
+                   name="radio-button"
+                   checked= {radioIdx === index }
+                   onChange={() => setIndex(radioIdx)}
+                 />
+               ))}
+                
+               
             </div>
           </div>
         </>
