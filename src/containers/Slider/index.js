dd3aef491f7f0632a0e1516bc8ae1df696f41462
samuel73000 <50187969+samuel73000@users.noreply.github.com>
2023-07-24 22:22:49 +0200

    724 event

--- src/containers/Slider/index.js
+++ src/containers/Slider/index.js
@@ -1,77 +1,62 @@
 import { useEffect, useState } from "react";
 import { useData } from "../../contexts/DataContext";
 import { displayMonth } from "../../helpers/Date";
-
 import "./style.scss";
-
-
 const Slider = () => {
   const { data } = useData();
   const [index, setIndex] = useState(0);
-  const [check, setCheck] = useState(false);
   const byDateDesc = data?.focus.sort((evtA, evtB) =>
     new Date(evtA.date) > new Date(evtB.date) ? -1 : 1
   );
   const nextCard = () => {
-  //   setTimeout(
-  //     length = 3 alors que max index = 2
-  //     // () => setIndex(index < byDateDesc.length-1 ? index+1  : 0),
-  //     5000
-  //   );
+    // length = 3 alors que max index = 2
+    setIndex(index < (byDateDesc && byDateDesc.length - 1) ? index + 1 : 0); // mis le timeout dans useeffect et mis le setindex conditionné aux lenghts qui doivent exister
   };
   useEffect(() => {
-    nextCard();
-  });
-const handleChangeRadio = (i) =>{
-  setIndex(i)
-  setCheck(true)
-}
-
-
-
-
-
-
+    const timeoutId = setTimeout(() => {
+      nextCard();
+    }, 5000);
+    return () => clearTimeout(timeoutId);
+  }, [nextCard]);
 
   return (
-    
     <div className="SlideCardList">
-      {byDateDesc?.map((event , idx) => (
-        <>
-          <div
-            key={event.title}
-            className={`SlideCard SlideCard--${
-              index === idx ? "display" : "hide"
-            }`}
-          >
-            <img src={event.cover} alt="forum" />
-            <div className="SlideCard__descriptionContainer">
-              <div className="SlideCard__description">
-                <h3>{event.title}</h3>
-                <p>{event.description}</p>
-                <div>{displayMonth(new Date(event.date))}</div>
-              </div>
+      {byDateDesc?.map((event, idx) => (
+        <div
+          key={event.title}
+          className={`SlideCard SlideCard--${
+            index === idx ? "display" : "hide"
+          }`}
+        >
+          <img src={event.cover} alt="forum" />
+          <div className="SlideCard__descriptionContainer">
+            <div className="SlideCard__description">
+              <h3>{event.title}</h3>
+              <p>{event.description}</p>
+              <div>{displayMonth(new Date(event.date))}</div>
             </div>
           </div>
-          <div className="SlideCard__paginationContainer">
-            <div className="SlideCard__pagination">
-            {byDateDesc?.map((_, radioIdx )  => (
-                 
-                 <input
-                 key={`${event.id}`}
-                   type="radio"
-                   name="radio-button"
-                   checked= {radioIdx === index || check }
-                   onChange={() => handleChangeRadio(radioIdx)}
-                 />
-               ))}
-                
-               
-            </div>
-          </div>
-        </>
+        </div>
       ))}
-    </div>
+      <div className="SlideCard__paginationContainer">
+        <div className="SlideCard__pagination">
+          {byDateDesc?.map(
+            (
+              event,// event au lieu d'underscore,
+              radioIdx 
+            ) => (
+              <input
+                key={event.title} // changé la prop pour en avoir une unique car erreur dans la console quand plusieurs ("$.id")
+                type="radio"
+                name="radio-button"
+                checked={radioIdx === index}
+                onChange={() => setIndex(radioIdx)} // ajouté onchange et changé la key et le checked sur index
+              />
+            )
+          )}
+        </div>
+      </div>
+    </div> // restructuré le html, lignes 40 et 25, pas deux fois "event" dans la même section
   );
 };
 
